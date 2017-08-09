e9a7d3a301d2fc997aacf37215eda0827db494ff
NingLuo <luo.n@husky.neu.edu>
2017-08-09 14:06:13 -0400

    Added PageDTO, PageVM, Page controller and its index.html

---
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CmsShoppingCart.Models.Data;
+using CmsShoppingCart.Models.ViewModels.Pages;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            // Declare a list of PageVM
+            List<PageVM> pagesList;
+
+            // Init the list
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToList().OrderBy(p => p.Sorting).Select(p => new PageVM(p)).ToList();
+            }
+            // return view with list
+            return View(pagesList);
+        }
+    }
+}
